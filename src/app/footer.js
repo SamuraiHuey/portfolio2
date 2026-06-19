727c42be64854402dad1b2c5cf9e4a4d7296a79b
Will B <tryarc@comcast.net>
2026-06-19 15:52:28 -0400

    centered footer

--- src/app/footer.js
+++ src/app/footer.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer>
-            <div className="flex gap-3 justify-self-end mr-3">
+            <div className="flex gap-3 justify-self-center">
                 <Link href="mailto:tryarc@comcast.net" target="_blank" className="group">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="h-10 w-10 fill-black-400 group-hover:fill-[#A9A9A9] ease-in-out duration-200" viewBox="0 0 16 16">
                         <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
