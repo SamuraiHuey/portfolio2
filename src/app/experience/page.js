bed932b6431b15775bddc3fcdd2751950de3ad0e
Will B <tryarc@comcast.net>
2026-06-19 13:45:05 -0400

    Fixed experience card desktop sizing

--- src/app/experience/page.js
+++ src/app/experience/page.js
@@ -8,8 +8,8 @@ export default function Home() {
       <h1 className="title">Experience</h1>
       <p className="p-5">I am a highly adaptable person who has worn a lot of different hats in my life. I have worked in every field from Food Service, Management, Sales, and have settled into a career as a Software Developer after completing the Coding Bootcamp at The University of Richmond. I came to this career thanks to my deep love of learning new things and understanding how things work. I am passionate about growing my knowledge and becoming the best developer I can be through first hand experience and traditional learning.</p>
 
-      <section className="flex grid-cols-3 items-top">
-        <div className="p-5">
+      <section className="flex grid-cols-3 justify-center items-top">
+        <div className="p-5 w-3/10">
           <Image
             className=""
             src={Headshot}
@@ -17,7 +17,7 @@ export default function Home() {
           <h2 className="mt-5">Full-Stack Development</h2>
           <p>I have a talent for communication that many in the software development world tend to lack, and this has been highly valuable in my work as a developer. I have the skills to understand user issues, how to address them professionally, and then communicate them in a way that other Developers and Engineers can understand. This is also key in communication with stakeholders who technically minded professionals often struggle with communicating with. I have also been responsible for the full-stack development of multiple company websites.</p>
         </div>
-        <div className="p-5">
+        <div className="p-5 w-3/10">
           <Image
             className=""
             src={Headshot}
@@ -25,7 +25,7 @@ export default function Home() {
           <h2 className="mt-5">Marketing</h2>
           <p>I have overseen various online marketing initiatives, including the development and implementation of major SEO and AI-SEO workflows that brought significant improvements to sites that were implementing them, as well as launched AI powered geofencing/geotargeting campaigns that saw major increases to online exposure. I also have helped individual sales producers elevate their personal brand view email marketing and strategic content pipelines.</p>
         </div>
-        <div className="p-5">
+        <div className="p-5 w-3/10">
           <Image
             className=""
             src={Headshot}
